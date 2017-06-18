2f42d7789c969be88441df4e58356a273353dee6
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2017-06-18 19:45:06 +0900

    file validate

--- reader.py
+++ reader.py
@@ -1,3 +1,5 @@
+import os.path
+import sys
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
@@ -11,7 +13,16 @@ laparams = LAParams()
 laparams.detect_vertical = True
 device = TextConverter(rsrcmgr, rettxt, codec='utf-8', laparams=laparams)
 # 処理するPDFを開く
-fp = open('sample.pdf', 'rb')
+argvs = sys.argv
+if len(argvs) != 2:
+    print('please input file path.')
+    quit()
+path = argvs[1]
+if not os.path.exists(path):
+    print(path + ' is invalid file path.')
+    quit()
+
+fp = open(path, 'rb')
 interpreter = PDFPageInterpreter(rsrcmgr, device)
 
 # maxpages：ページ指定（0は全ページ）
